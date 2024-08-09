10ed9032fd50dbfef96e990df4a3fa53a087d7d1
Kushal1715 <kushalkhadka77@gmail.com>
2024-08-09 10:49:42 +0545

    lab1_2 completed

---
+++ LabReportKushal3/app/src/main/java/com/example/labreportkushal/Lab_1_2.java
@@ -0,0 +1,13 @@
+// Java
+package com.example.labreportkushal;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lab_1_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab12);
+    }
+}
