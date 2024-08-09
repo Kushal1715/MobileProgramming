9d88d5c4118cb9afd9d26377fdc4436fd0b00c17
Kushal1715 <kushalkhadka77@gmail.com>
2024-08-09 18:28:41 +0545

    lab 1 question no 3 completed

--- LabReportKushal3/app/src/main/java/com/example/labreportkushal/Lab_1q3.java
+++ LabReportKushal3/app/src/main/java/com/example/labreportkushal/Lab_1q3.java
@@ -3,10 +3,11 @@ package com.example.labreportkushal;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Lab_1_2 extends AppCompatActivity {
+public class Lab_1q3 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lab12);
+        setContentView(R.layout.activity_lab1q3);
     }
 }
